abdf5f01e40329b7b2d97bf3803c87254008c851
Okubyona <andrewbazua@gmail.com>
2019-11-06 00:07:03 -0800

    Initial commit

---
+++ source/main.c
@@ -0,0 +1,26 @@
+/*	Author: abazu001
+ *  Partner(s) Name:
+ *	Lab Section:
+ *	Assignment: Lab #8  Exercise #4
+ *	Exercise Description: [ Design a system, using a bank of eight LEDs, where
+            the number of LEDs illuminated is a representation of how much light
+            is detected. For example, when more light is detected, more LEDs are
+            illuminated. ]
+ *
+ *	I acknowledge all content contained herein, excluding template or example
+ *	code, is my own original work.
+ */
+#include <avr/io.h>
+#ifdef _SIMULATE_
+#include "simAVRHeader.h"
+#endif
+
+int main(void) {
+    /* Insert DDR and PORT initializations */
+
+    /* Insert your solution below */
+    while (1) {
+
+    }
+    return 1;
+}
